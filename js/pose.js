5ddb6eb95bb61c01b20e9f047ac1948bf3a4dfd0
1092024 <80467139+1092024@users.noreply.github.com>
2024-03-27 19:49:18 +0800

    Add files via upload

--- js/pose.js
+++ js/pose.js
@@ -1,13 +1,33 @@
-const video5 = document.getElementsByClassName('input_video5')[0];
-const out5 = document.getElementsByClassName('output5')[0];
-const controlsElement5 = document.getElementsByClassName('control5')[0];
-const canvasCtx5 = out5.getContext('2d');
+function startVideo() {
+  document.getElementById("btnPlay").style.display = "none";
+  var video = document.getElementById("myVideo");
+  video.style.display = "block";
+  video.play();
+}
+
+function videoEnded() {
+  var video = document.getElementById("myVideo");
+  video.classList.add("fade-out");
+  // video.style.display = "none";
+  // alert("前導播放完畢！");
+  // 使用Promise和async/await创建延迟
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  (async () => {
+    await delay(2000); // 这里的1000表示延迟1秒，您可以根据需要调整延迟的时间
+    pose.onResults(onResultsPose);
+  })();
+}
+const video5 = document.getElementsByClassName("input_video5")[0];
+const out5 = document.getElementsByClassName("output5")[0];
+const controlsElement5 = document.getElementsByClassName("control5")[0];
+const canvasCtx5 = out5.getContext("2d");
 
 const fpsControl = new FPS();
 
-const spinner = document.querySelector('.loading');
+const spinner = document.querySelector(".loading");
 spinner.ontransitionend = () => {
-  spinner.style.display = 'none';
+  spinner.style.display = "none";
 };
 
 function zColor(data) {
@@ -48,46 +68,47 @@ function fadeOutOverlayImages() {
 }
 
 function onResultsPose(results) {
-  document.body.classList.add('loaded');
+  document.body.classList.add("loaded");
   fpsControl.tick();
 
   // 清除上一幀的內容
   canvasCtx5.clearRect(0, 0, out5.width, out5.height);
 
   // for (let i = 0; i < results.poseLandmarks.length; i++) {
-    const landmark = results.poseLandmarks[0];
-    const x = landmark.x * out5.width * 5;
-    const y = landmark.y * out5.height * 5;
+  const landmark = results.poseLandmarks[0];
+  const x = landmark.x * out5.width * 5;
+  const y = landmark.y * out5.height * 5;
 
-    // 創建一個新的overlayImage元素
-    const overlayImage = document.createElement('img');
-    overlayImage.src = './img/dot.png'; // 設置疊加圖像的路徑
-    overlayImage.style.position = 'absolute';
+  // 創建一個新的overlayImage元素
+  const overlayImage = document.createElement("img");
+  overlayImage.src = "./img/dot.png"; // 設置疊加圖像的路徑
+  overlayImage.style.position = "absolute";
 
-    // 設置疊加圖像的位置
-    overlayImage.style.left = `${x}px`;
-    overlayImage.style.top = `${y}px`;
+  // 設置疊加圖像的位置
+  overlayImage.style.left = `${x}px`;
+  overlayImage.style.top = `${y}px`;
 
-    // 設置疊加圖像的大小
-    overlayImage.style.width = '15px';
-    overlayImage.style.height = '15px';
+  // 設置疊加圖像的大小
+  overlayImage.style.width = "15px";
+  overlayImage.style.height = "15px";
 
-    // 將疊加圖像放到頁面上
-    document.body.appendChild(overlayImage);
+  // 將疊加圖像放到頁面上
+  document.body.appendChild(overlayImage);
 
-    // 將疊加圖像儲存到陣列中
-    overlayImages.push(overlayImage);
+  // 將疊加圖像儲存到陣列中
+  overlayImages.push(overlayImage);
 
-    // 繪製連接線和關鍵點
-    drawConnectors(
-      canvasCtx5, [landmark], POSE_CONNECTIONS, {
-        color: (data) => {
-          return '#FF0000'; 
-        }
-      });
+  // 繪製連接線和關鍵點
+  drawConnectors(canvasCtx5, [landmark], POSE_CONNECTIONS, {
+    color: (data) => {
+      return "#FF0000";
+    },
+  });
 
-    drawLandmarks(
-      canvasCtx5, [landmark], {color: zColor, fillColor: '#FF0000'}); // 繪製關鍵點
+  drawLandmarks(canvasCtx5, [landmark], {
+    color: zColor,
+    fillColor: "#FF0000",
+  }); // 繪製關鍵點
   // }
 
   // 繪製攝影機圖像
@@ -97,52 +118,49 @@ function onResultsPose(results) {
   fadeOutOverlayImages();
 }
 
-
-const pose = new Pose({locateFile: (file) => {
-  return `https://cdn.jsdelivr.net/npm/@mediapipe/pose@0.2/${file}`;
-}});
-pose.onResults(onResultsPose);
+const pose = new Pose({
+  locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/pose@0.2/${file}`;
+  },
+});
+// pose.onResults(onResultsPose);
 
 const camera = new Camera(video5, {
   onFrame: async () => {
-    await pose.send({image: video5});
+    await pose.send({ image: video5 });
   },
   width: 1080,
-  height: 720
+  height: 720,
 });
 camera.start();
 
 new ControlPanel(controlsElement5, {
-      selfieMode: true,
-      upperBodyOnly: false,
-      smoothLandmarks: true,
-      minDetectionConfidence: 0.5,
-      minTrackingConfidence: 0.5
-    })
-    .add([
-      new StaticText({title: 'MediaPipe Pose'}),
-      fpsControl,
-      new Toggle({title: 'Selfie Mode', field: 'selfieMode'}),
-      new Toggle({title: 'Upper-body Only', field: 'upperBodyOnly'}),
-      new Toggle({title: 'Smooth Landmarks', field: 'smoothLandmarks'}),
-      new Slider({
-        title: 'Min Detection Confidence',
-        field: 'minDetectionConfidence',
-        range: [0, 1],
-        step: 0.01
-      }),
-      new Slider({
-        title: 'Min Tracking Confidence',
-        field: 'minTrackingConfidence',
-        range: [0, 1],
-        step: 0.01
-      }),
-    ])
-    .on(options => {
-      video5.classList.toggle('selfie', options.selfieMode);
-      pose.setOptions(options);
-    });
-
-    
-
-    
+  selfieMode: true,
+  upperBodyOnly: false,
+  smoothLandmarks: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5,
+})
+  .add([
+    new StaticText({ title: "MediaPipe Pose" }),
+    fpsControl,
+    new Toggle({ title: "Selfie Mode", field: "selfieMode" }),
+    new Toggle({ title: "Upper-body Only", field: "upperBodyOnly" }),
+    new Toggle({ title: "Smooth Landmarks", field: "smoothLandmarks" }),
+    new Slider({
+      title: "Min Detection Confidence",
+      field: "minDetectionConfidence",
+      range: [0, 1],
+      step: 0.01,
+    }),
+    new Slider({
+      title: "Min Tracking Confidence",
+      field: "minTrackingConfidence",
+      range: [0, 1],
+      step: 0.01,
+    }),
+  ])
+  .on((options) => {
+    video5.classList.toggle("selfie", options.selfieMode);
+    pose.setOptions(options);
+  });
